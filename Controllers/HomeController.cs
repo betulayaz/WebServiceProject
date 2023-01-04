a33b8a9848e423be24a8f966052ac3b60a5de484
betulayaz <btl.ayazz41@gmail.com>
2023-01-04 14:02:05 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebServiceProject.ServiceReference1;
+
+namespace WebServiceProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private CountryInfoServiceSoapTypeClient request = new CountryInfoServiceSoapTypeClient("CountryInfoServiceSoap");
+        public ActionResult Index()
+        {           
+            var countryName = request.ListOfCountryNamesByCode();
+            List<SelectListItem> items = new List<SelectListItem>();
+            for (int i=0;i< countryName.Length;i++)
+            {
+                items.Add(new SelectListItem { Text = "" + countryName[i].sName + "", Value = "" + countryName[i].sISOCode + "" });
+            }
+            
+            ViewBag.country = items;
+
+            return View();
+        }
+        [Route("getcapitalcity-{countycode?}")]
+        public ActionResult GetCapitalCity(string countycode)
+        {
+            var capitalCity = request.CapitalCity(countycode);
+
+            return Json(capitalCity);
+        }
+        [Route("getcountrycode-{county?}")]
+        public ActionResult GetCountryCode(string county)
+        {
+            var countryIsoCode = request.CountryISOCode(county);
+
+            return Json(countryIsoCode);
+        }
+        [Route("getcountrycurrency-{countycode?}")]
+        public ActionResult GetCountryCurrency(string countycode)
+        {
+            var countrycurrency = request.CountryCurrency(countycode);
+
+            return Json(countrycurrency);
+        }
+        [Route("createdb")]
+        public ActionResult CreateDb()
+        {
+            if (Request["type"] == "add")
+            {
+                dbConnection db = new dbConnection();
+                DataTable dt = null;
+                string country_id,country_name = "", country_capital_city = "", country_code = "", country_currency = "";
+                country_id = Guid.NewGuid().ToString();
+                country_name = Request["country_name"].ToString();
+                country_capital_city = Request["country_capital_city"].ToString();
+                country_code = Request["country_code"].ToString();
+                country_currency = Request["country_currency"].ToString();
+                dt = db.InsertCountry(country_id,country_name, country_capital_city, country_code, country_currency, DateTime.Now);
+                if (dt != null)
+                {
+                    Response.Write("true");
+                    return View();
+                }
+                else
+                {
+                    Response.Write("false");
+                    return View();
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ dbConnection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebServiceProject
+{
+    public class dbConnection
+    {
+        private SqlConnection con;
+        private void connection()
+        {
+            string cnnStr = System.Configuration.ConfigurationManager.AppSettings["db"];
+            con = new SqlConnection(cnnStr);
+
+        }
+        public DataTable InsertCountry(string country_id, string country_name, string country_capital_city, string country_code, string country_currency, DateTime created_datetime)
+        {
+            connection();
+            SqlCommand com = new SqlCommand("sp_InsertCountry", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@country_id", country_id);
+            com.Parameters.AddWithValue("@country_name", country_name);
+            com.Parameters.AddWithValue("@country_capital_city", country_capital_city);
+            com.Parameters.AddWithValue("@country_code", country_code);
+            com.Parameters.AddWithValue("@country_currency", country_currency);
+            com.Parameters.AddWithValue("@created_datetime", created_datetime);
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+            con.Open();
+            da.Fill(dt);
+            con.Close();
+            return dt;
+        }
+    }
+}
